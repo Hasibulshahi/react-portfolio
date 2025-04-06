5caa7c2a9794240d3ae2e431ba45aa8a16a1193b
Hasibul Hassan <shahi_aust@hotmail.com>
2025-04-06 13:38:26 +0800

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,194 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Md Hasibul Hassan",
+  tagline: "Driven by Code. Powered by Passion. Focused on Results.",
+  img: profile,
+  about: `Versatile Full-Stack & Mobile App Developer with 6+ years of experience, specializing in Flutter, React Native, Kotlin, Java, ASP.Net Core, and cloud-based architecture on Azure & AWS. Proven track record in developing scalable web and mobile applications, leading cross-functional teams, and delivering high-impact solutions across fintech, e-commerce, and AI-driven platforms. Adept at clean code architecture, API integration, and modern development frameworks. Passionate about innovation, continuous learning, and building seamless digital experiences.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Senior Java Backend Developer",
+    Company: `Accord Innovation Sdn Bhd`,
+    Client: `Al Rajhi Bank`,
+    Location: "Kuala Lumpur, Malaysia",
+    Type: "Full Time",
+    Duration: "Feb 2025 - Present",
+  },
+  {
+    Position: "Full Stack Developer",
+    Company: `Tella`,
+    Client: `In House Project`,
+    Location: "Kuala Lumpur, Malaysia",
+    Type: "Full Time",
+    Duration: "May 2024 - Feb 2025",
+  },
+  {
+    Position: "Application Development Sr Analyst",
+    Company: `Accenture`,
+    Client: `UOB Bank`,
+    Location: "Kuala Lumpur, Malaysia",
+    Type: "Full Time",
+    Duration: "Nov 2022 - May 2024",
+  },
+  {
+    Position: "Mobile Application Developer",
+    Company: `Tractive Sdn Bhd`,
+    Client: `In House Project`,
+    Location: "Kuala Lumpur, Malaysia",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Oct 2022",
+  },
+  {
+    Position: "Junior Software Engineer",
+    Company: `Socian AI`,
+    Client: `Uniliver Bangladesh`,
+    Location: "Dhaka, Bangladesh",
+    Type: "Full Time",
+    Duration: "Jun 2016 - Jul 2019",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Master of Science",
+    Company: "University Putra Malaysia",
+    Location: "Kuala Lumpur, Malaysia",
+    Type: "Full Time",
+    Duration: "Nov 2019 - Dec 2021",
+  },
+  {
+    Position: "Bachelor in Computer Science & Engineering",
+    Company: "Ahsanullah University of Science & Technology",
+    Location: "Dhaka, Bangladesh",
+    Type: "Full Time",
+    Duration: "Apr 2013 - Jun 2017",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "hi@example.com",
+  phone: "+91 12345 67890",
+};
